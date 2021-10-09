976e4e49863c9301ebc91fb78e819e2647173ea7
faraibernard <73079300+faraibernard@users.noreply.github.com>
2021-10-09 17:03:54 -0400

    Add files via upload

---
+++ job_instant/__manifest__.py
@@ -0,0 +1,24 @@
+
+# -*- coding: utf-8 -*-
+###################################################################################
+#    
+#
+###################################################################################
+
+{
+    'name': 'Job Instant',
+    'version': '15.0.0.0.0',
+    'author': 'Centralasis Proactive Solutions',
+    'maintainer': 'Centralasis Proactive Solutions',
+    'company': 'Centralasis Proactive Solutions',
+    'website': 'https://www.centralasis.com',
+    'depends': ['base', 'hr_payroll'
+                ],
+    'data' : [
+        'views/work_entry.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
